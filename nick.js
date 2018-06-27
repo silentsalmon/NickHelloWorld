ea1d872f5346e89563230970c163699a0ddf8f83
silentsalmon <40640855+silentsalmon@users.noreply.github.com>
2018-06-27 13:13:36 -0500

    First Examples

---
+++ nick.js
@@ -0,0 +1,49 @@
+let justin = 11;
+
+/*
+//if example
+if(justin === 10 )
+{
+    console.log("funnybunny");
+}
+else if( justin === 11 )
+{
+    console.log("dirtybunny");
+}
+else
+{
+    console.log("uglybunny");
+}*/
+//switch example
+/*
+switch(justin)
+{
+    case 1:
+        console.log("beans");
+    break;
+    case 2:
+    case 3:
+    case 4:
+        console.log("uglybunny");
+        break;
+    case 5:
+    case 6:
+        console.log("dirtybunny");
+        break;
+
+    case 7:
+    case 8:
+    case 9:
+        console.log("funnybunny");
+        break;
+    case 10:
+        console.log("fuuuuu");
+        break;
+    default:
+        console.log("blacknick");
+        break;
+}*/
+//arrray example
+let nick = [5, 8, 9];
+
+console.log(nick[2].toString());
